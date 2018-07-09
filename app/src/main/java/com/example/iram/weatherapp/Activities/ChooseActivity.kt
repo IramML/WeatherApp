f21c084eb179da1f4322c53279abd701ee80a85e
IramML <IramMartinezLamas@gmail.com>
2018-07-09 10:30:40 -0500

    Integrate Google maps API

--- app/src/main/java/com/example/iram/weatherapp/Activities/ChooseActivity.kt
+++ app/src/main/java/com/example/iram/weatherapp/Activities/ChooseActivity.kt
@@ -1,8 +1,11 @@
 package com.example.iram.weatherapp.Activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 import com.example.iram.weatherapp.R
 
 class ChooseActivity : AppCompatActivity() {
@@ -14,5 +17,24 @@ class ChooseActivity : AppCompatActivity() {
         toolbar=findViewById(R.id.actionBarChoose)
         toolbar?.setTitle(R.string.app_name)
         setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_choose, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.icMap->{
+                val intent =Intent(this, MapsActivity::class.java)
+                startActivity(intent
+                )
+                return true
+            }
+            else ->return super.onOptionsItemSelected(item)
+
+        }
+
     }
 }
